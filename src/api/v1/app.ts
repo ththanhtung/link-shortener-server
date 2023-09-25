0bd9d4e1e22e9daa16c31d8ee403118afe200fd1
ththanhtung <nguyenththanhtung317@gmail.com>
2023-09-25 15:51:31 +0700

    feat: add helmet and compression

--- src/api/v1/app.ts
+++ src/api/v1/app.ts
@@ -2,10 +2,19 @@ import express from 'express'
 import { dbInstance } from '../../database/init.database'
 import { errorsHandler } from './middlewares/errorsHandler'
 import router from './routes'
+import cors from 'cors'
+import helmet from 'helmet'
+import compression from 'compression';
 
 const app = express()
 
 app.use(express.json())
+app.use(helmet())
+app.use(compression())
+
+app.use(cors({
+    origin: '*'
+}))
 
 
 // init database
